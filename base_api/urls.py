b12e9e88983a7ae41cb1480e44bc7bc3c92c0b25
AlphaTechnic <alphatechnicist@gmail.com>
2022-03-22 21:24:26 +0900

    [refactor] 폴더명변경: TartProject -> base_api

--- base_api/urls.py
+++ base_api/urls.py
@@ -1,4 +1,4 @@
-"""TartProject URL Configuration
+"""base_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
